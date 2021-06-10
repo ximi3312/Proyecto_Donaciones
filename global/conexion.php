cf641fe1195323e310c0095f46736057d6dfc4cf
ximi3312 <a19110093@ceti.mx>
2021-06-10 00:23:53 -0500

    cambiar o eliminar

--- global/conexion.php
+++ global/conexion.php
@@ -1,11 +1,13 @@
 <?php
-$servidor="mysql:dbname=".BD.";host=".SERVIDOR; 
 
-try{
-    $pdo= new PDO($servidor,USUARIO,PASSWORD,
-        array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
-        echo "<script>alert('Conectado...')</script>";
-}catch(PDOException $e){
-    echo "<script>alert('Hecho...')</script>";
+   try{
+
+	   $pdo= new PDO('mysql:host=localhost;dbname=tienda', 'root', '',
+                     array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+	    //echo "<script>alert('Conectado!')</script>";
+
+    }catch(PDOException $e){
+	//echo "<script>alert('Error...')</script>";
 }
+
 ?>
